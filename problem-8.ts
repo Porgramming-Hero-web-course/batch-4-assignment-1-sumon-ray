7696074ad8a4f5b106fb87153c3add96690904e6
sumon-ray <raysumon220020@gmail.com>
2024-11-08 20:18:45 +0600

    problem-6 solved

--- problem-8.ts
+++ problem-8.ts
@@ -2,6 +2,5 @@ function validateKeys<T extends object>(obj: T, keys: (keyof T)[]): boolean {
   return keys.every((key) => key in obj);
 }
 
-// Sample Input
-const person2 = { name: "Alice", age: 25, email: "alice@example.com" };
-console.log(validateKeys(person2, ["name", "age", "email"]));
+
+
